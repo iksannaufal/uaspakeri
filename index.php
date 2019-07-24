89076f4063760850354e6049b929cd6df32b99be
Muhammadakhlakul <48430637+Muhammadakhlakul@users.noreply.github.com>
2019-07-24 21:02:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
   // langsung redirect ke halaman tampil mahasiswa
-  header("Location: https://akhlakulaziz27.000webhostapp.com/weservuas/appcordova-master/www/index2.html");
+  header("Location: https://akhlakulaziz27.000webhostapp.com/weservuas/appcordova-master/www/index.html");
 ?>
